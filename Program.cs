cc21dc2ffa91df8b08c9b47fc29b971d6cd47ec9
raccoon <donogo.umrahil@yandex.ru>
2023-08-14 19:57:06 +0300

    okay its commit

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@
 //44 5 78 -> 78
 //22 3 9 -> 22
 
+using System.ComponentModel.DataAnnotations;
+
 Console.WriteLine ("Введите первое число");
 int number1 = Convert.ToInt32(Console.ReadLine());
 
@@ -12,35 +14,8 @@ int number2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine ("Введите третье число");
 int number3 = Convert.ToInt32(Console.ReadLine());
 
-if (number1 > number2 >> number3);
-{
-    Console.WriteLine("Максимальное число:");
-    Console.WriteLine(number1);
+if (number1 > number2 && number1 > number3) Console.WriteLine(number1);
 
-else
-{
-    if (number2 > number1 >> number3)
-    {
-        Console.WriteLine("Максимальное число:");
-        Console.WriteLine(number2);
-    }
-}
-}
+else if (number2 > number1 && number2 > number3) Console.WriteLine(number2);
 
-
-
-
-
-
-
-
-//else if (number2 > number1 >> number3);
-//{
-//  Console.WriteLine("Максимальное число:");
-// Console.WriteLine(number2);
-// if (number3 > number1 >> number2);
-// {
-//      Console.WriteLine("Максимальное число:");
-// Console.WriteLine(number3);
-//}
-//}
+else if (number3 > number1 && number3 > number2) Console.WriteLine(number3);
